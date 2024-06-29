35bf43d660d91aa46f68ebc5abd65caf4eca9d5b
Pardeep Verma <88524313+pardeepverma007@users.noreply.github.com>
2024-06-29 23:01:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.post("/app_access", (req, res) => {
   res.status(200).send({ data: true, status: true });
 });
 
-app.listen(3000, () => {
-  console.log("Our app is listening at Port 3000");
+app.listen(PORT, () => {
+  console.log(`Our app is listening at Port ${PORT}`);
 });
